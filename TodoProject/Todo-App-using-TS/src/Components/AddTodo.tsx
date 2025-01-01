aa480d3d2c1d0666ba4b795a3539a4214692a599
Ritika Shishodia <ritika18-cs@sanskar.org>
2025-01-01 10:11:43 +0530

    Learning ts : edit to do feature

--- TodoProject/Todo-App-using-TS/src/Components/AddTodo.tsx
+++ TodoProject/Todo-App-using-TS/src/Components/AddTodo.tsx
@@ -6,7 +6,9 @@ import { Todo } from "./Todo";
 
 export const AddTodo: FC = () => {
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [todoFlag, setTodoFlag] = useState<boolean>(false);
   const [task, setTask] = useState<string>("");
+  const [editedId, setEditedId] = useState<string>("");
   // set Task description
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setTask(event.target.value);
@@ -23,13 +25,41 @@ export const AddTodo: FC = () => {
     setTask("");
   };
 
-  
   //deleteTodo
   const deleteTodo = (taskId: string): void => {
-    setTaskList([...taskList.filter((task) =>
-     task.id !== taskId )
-    ])
+    setTaskList([...taskList.filter((task) => task.id !== taskId)]);
   };
+
+  //edit todo
+  const editTodo = (taskId: string): void => {
+    const task = taskList.find((task) => task.id === taskId);
+    setTask(task ? task.taskDescription : "");
+    setTodoFlag(true);
+    setEditedId(taskId);
+  };
+
+  //update edit to do
+  const updateEditTodo = () => {
+   const updatedList:(ITask)[] = taskList.map((tempTask) => {
+    if(tempTask.id === editedId){
+
+        const updateTask:ITask = {
+            taskDescription : task,
+            id : tempTask.id,
+            isCompleted:tempTask.isCompleted,
+            
+        }
+        return updateTask
+    }
+    else 
+     return tempTask;
+})
+    setTaskList(updatedList);
+    setTask("");
+    setTodoFlag(false);
+  };
+
+
 
   return (
     <>
@@ -45,18 +75,23 @@ export const AddTodo: FC = () => {
             className="rounded-md text-3xl px-4 w-full focus:outline-none focus:ring-2 bg-blue-950 bg-opacity-50 transition duration-200  "
           />
           <button
-            onClick={addTodo}
+            onClick={todoFlag ? updateEditTodo :addTodo}
             disabled={task === ""}
             className="bg-blue-950 hover:ring-2 border-blue-400 hover:opacity-80 text-3xl p-4 rounded-md"
           >
-            Add Task
+            {todoFlag ? "Edit" : "Add"} Task
           </button>
         </div>
         {/* show all todos */}
       </div>
       <div className=" mx-auto w-1/2">
         {taskList.map((task) => (
-          <Todo task={task} key={task.id} deleteTodo={deleteTodo} />
+          <Todo
+            task={task}
+            key={task.id}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
         ))}
       </div>
     </>
--- TodoProject/Todo-App-using-TS/src/Components/Todo.tsx
+++ TodoProject/Todo-App-using-TS/src/Components/Todo.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from "react";
 import { ITask } from "../Interfaces/ITask";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   task: ITask;
   deleteTodo(taskId: string): void;
+  editTodo(taskId:string):void;
 }
 
-export const Todo = ({ task, deleteTodo }: Props) => {
+export const Todo = ({ task, deleteTodo ,editTodo}: Props) => {
   return (
     <div
       className="bg-blue-950 bg-opacity-20 hover:ring-2 my-4 p-4 rounded-md text-2xl"
@@ -16,13 +17,22 @@ export const Todo = ({ task, deleteTodo }: Props) => {
     >
       <div className="flex justify-between text-center align-middle">
         <p className="my-auto">{task.taskDescription}</p>
+        <div className="gap-3 flex">
         <button
           type="button"
           className="hover:shadow hover:shadow-slate-300 p-4 border rounded-md"
           onClick={() => deleteTodo(task.id)}
         >
-          <FontAwesomeIcon icon={faTrash} color="white" />
+          <FontAwesomeIcon icon={faTrash} color="red" />
         </button>
+        <button
+          type="button"
+          className="hover:shadow hover:shadow-slate-300 p-4 border rounded-md"
+          onClick={() => editTodo(task.id)}
+        >
+          <FontAwesomeIcon icon={faEdit} color="green" />
+        </button>
+        </div>
       </div>
     </div>
   );
